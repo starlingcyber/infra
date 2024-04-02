9b4818268383bcaa63a3f2c4136469eaab0803d2
finch <finch@starlingcyber.net>
2024-04-02 19:07:23 -0400

    More pleasing indentation

--- cometbft.nix
+++ cometbft.nix
@@ -6,14 +6,15 @@ let
   hash = "sha256-wNVHsifieAtZgedavCEJLgG0kRDqUhG4Lk5ciTPoNzI=";
   vendorHash = "sha256-JPEGMa0HDesEtKFvgLUP2UfTB0DlParepE2p+n06Igc=";
 in with pkgs; with pkgs.lib;
-  (buildGoModule rec {
-    pname = name;
-    inherit version vendorHash;
-    subPackages = [ "cmd/${name}" ];
-    src = fetchFromGitHub {
-      owner = name;
-      repo = name;
-      rev = "v${version}";
-      inherit hash;
-    };
-  }).overrideAttrs (_: { doCheck = false; }) # Disable tests to improve build times
+
+(buildGoModule rec {
+  pname = name;
+  inherit version vendorHash;
+  subPackages = [ "cmd/${name}" ];
+  src = fetchFromGitHub {
+    owner = name;
+    repo = name;
+    rev = "v${version}";
+    inherit hash;
+  };
+}).overrideAttrs (_: { doCheck = false; }) # Disable tests to improve build times
--- horcrux.nix
+++ horcrux.nix
@@ -6,18 +6,19 @@ let
   hash = "sha256-ECrIFCJ0vIfuVfJmsGNRvwsf2lLLXhrZ5OCR300Qn0I=";
   vendorHash = "sha256-+fArmL2NwkSDo7jHN/wXu9mff0mXuXu2MtmXjuT1W0E=";
 in with pkgs; with pkgs.lib; with builtins;
-  (buildGoModule rec {
-    pname = name;
-    inherit version vendorHash;
-    subPackages = [ "cmd/${name}" ];
-    src = filterSource
-      # Workaround for bug preventing Go 1.22 from playing nicely with buildGoModule when
-      # using go.work workspace -- we don't need it because we just want the executable
-      (path: type: builtins.match ".*go.work$" path == null)
-      (fetchFromGitHub {
-        owner = "strangelove-ventures";
-        repo = name;
-        rev = "v${version}";
-        inherit hash;
-      });
-  }).overrideAttrs (_: { doCheck = false; }) # Disable tests to improve build times
+
+(buildGoModule rec {
+  pname = name;
+  inherit version vendorHash;
+  subPackages = [ "cmd/${name}" ];
+  src = filterSource
+    # Workaround for bug preventing Go 1.22 from playing nicely with buildGoModule when
+    # using go.work workspace -- we don't need it because we just want the executable
+    (path: type: builtins.match ".*go.work$" path == null)
+    (fetchFromGitHub {
+      owner = "strangelove-ventures";
+      repo = name;
+      rev = "v${version}";
+      inherit hash;
+    });
+}).overrideAttrs (_: { doCheck = false; }) # Disable tests to improve build times
