a0ca81ecf8149d7446e22fc06c53cd228420ad82
finch <finch@starlingcyber.net>
2024-04-03 22:31:41 -0400

    Make flake slightly more concise, add tf devshell

--- cometbft.nix
+++ cometbft.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import <nixpkgs> {}, ... }: let
+{ pkgs ? import <nixpkgs> {} }: let
 
 name = "cometbft";
 owner = "cometbft";
--- flake.nix
+++ flake.nix
@@ -12,14 +12,15 @@
 
   outputs = inputs @ { self, nixpkgs, flake-parts, crane, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      flake = {};
       systems = [ "x86_64-linux" ];
+      flake = {};
       perSystem = { config, self', inputs', pkgs, system, ... }:
         with pkgs; let
           cometbft = callPackage ./cometbft.nix { inherit pkgs; };
           horcrux = callPackage ./horcrux.nix { inherit pkgs; };
           penumbra = callPackage ./penumbra.nix { inherit pkgs crane; };
         in {
+          devShells.default = callPackage ./shell.nix { inherit pkgs; };
           packages = {
             inherit cometbft horcrux penumbra;
             default = symlinkJoin {
@@ -28,19 +29,13 @@
             };
           };
           apps = {
-            pd = {
-              type = "app";
-              program = "${penumbra}/bin/pd";
-            };
-            pcli = {
-              type = "app";
-              program = "${penumbra}/bin/pcli";
-            };
-            pclientd = {
-              type = "app";
-              program = "${penumbra}/bin/pclientd";
-            };
+            pd = { type = "app"; program = "${penumbra}/bin/pd"; };
+            pcli = { type = "app"; program = "${penumbra}/bin/pcli"; };
+            pclientd = { type = "app"; program = "${penumbra}/bin/pclientd"; };
           };
-        };
+
+          # Permits unfree licenses, thank you to <https://jamesconroyfinn.com/til/flake-parts-and-unfree-packages>
+          _module.args.pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
     };
+  };
 }
