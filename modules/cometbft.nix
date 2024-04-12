36d003f1cb11b336ccda649a3d39abcc07c2fce2
finch <finch@starlingcyber.net>
2024-04-12 18:15:01 +0000

    Add more security hardening

--- modules/cometbft.nix
+++ modules/cometbft.nix
@@ -2,6 +2,111 @@ self: { config, pkgs, lib, ...  }:
 
 with lib; let
   cfg = config.penumbra.services.cometbft;
+
+  # Shorthand for the package, used below
+  cometbft = self.packages.${pkgs.system}.cometbft;
+
+  # Write a config.toml file for CometBFT given the configuration options
+  configToml = (pkgs.formats.toml {}).generate "penumbra-cometbft-config" {
+    version = cometbft.version;
+    proxy_app = "tcp://${cfg.proxyApp.ip}:${toString cfg.proxyApp.port}";
+    moniker = cfg.moniker;
+    db_backend = "goleveldb";
+    db_dir = cfg.dataDir;
+    log_level = "info";
+    log_format = "plain";
+    priv_validator_laddr =
+      if cfg.privValidator.laddr.enable
+      then "tcp://${cfg.privValidator.laddr.ip}:${toString cfg.privValidator.laddr.port}"
+      else "";
+    abci = "socket";
+    filter_peers = false;
+    rpc = mkIf cfg.rpc.enable {
+      laddr = "tcp://${cfg.rpc.ip}:${toString cfg.rpc.port}";
+      cors_allowed_origins = [];
+      cors_allowed_methods = ["HEAD" "GET" "POST"];
+      cors_allowed_headers = ["Origin" "Accept" "Content-Type" "X-Requested-With" "X-Server-Time"];
+      grpc_laddr = "";
+      grpc_max_open_connections = 900;
+      unsafe = false;
+      max_open_connections = 900;
+      max_subscription_clients = 100;
+      max_subscriptions_per_client = 5;
+      timeout_broadcast_tx_commit = "10000ms";
+      max_body_bytes = 1000000;
+      max_header_bytes = 1048576;
+      tls_cert_file = "";
+      tls_key_file = "";
+      pprof_laddr = "";
+    };
+    p2p = {
+      laddr = "tcp://${cfg.p2p.ip}:${toString cfg.p2p.port}";
+      external_address = cfg.p2p.externalAddress;
+      seeds = concatStringsSep "," cfg.p2p.seeds;
+      persistent_peers = concatStringsSep "," cfg.p2p.persistentPeers;
+      addr_book_file = cfg.p2p.addrBook;
+      addr_book_strict = cfg.p2p.addrBookStrict;
+      max_num_inbound_peers = cfg.p2p.maxPeers.inbound;
+      max_num_outbound_peers = cfg.p2p.maxPeers.outbound;
+      unconditional_peer_ids = concatStringsSep "," cfg.p2p.unconditionalPeerIds;
+      persistent_peers_max_dial_period = "0s";
+      flush_throttle_timeout = "100ms";
+      max_packet_msg_payload_size = 1024;
+      send_rate = 5120000;
+      recv_rate = 5120000;
+      pex = cfg.p2p.pex;
+      seed_mode = cfg.p2p.seedMode;
+      private_peer_ids = concatStringsSep "," cfg.p2p.privatePeerIds;
+      allow_duplicate_ip = cfg.p2p.allowDuplicateIp;
+      handshake_timeout = cfg.p2p.handshakeTimeout;
+      dial_timeout = cfg.p2p.dialTimeout;
+    };
+    mempool = {
+      recheck = true;
+      broadcast = true;
+      wal_dir = "";
+      size = 5000;
+      max_txs_bytes = 1073741824;
+      cache_size = 10000;
+      keep-invalid-txs-in-cache = false;
+      max_tx_bytes = 1048576;
+      max_batch_bytes = 0;
+    };
+    consensus = {
+      wal_file = "${cfg.dataDir}/cs.wal/wal";
+      timeout_propose = "3000ms";
+      timeout_propose_delta = "500ms";
+      timeout_prevote = "1000ms";
+      timeout_prevote_delta = "500ms";
+      timeout_precommit = "1000ms";
+      timeout_precommit_delta = "500ms";
+      timeout_commit = "5000ms";
+      double_sign_check_height = cfg.consensus.doubleSignCheckHeight;
+      skip_timeout_commit = false;
+      create_empty_blocks = true;
+      create_empty_blocks_interval = "0ms";
+      peer_gossip_sleep_duration = "100ms";
+      peer_query_maj23_sleep_duration = "2000ms";
+    };
+    instrumentation = {
+      prometheus = toString cfg.prometheus.listenPort ? false;
+      prometheus_listen_addr = ":${toString cfg.prometheus.listenPort}";
+      max_open_connections = 3;
+      namespace = "cometbft";
+    };
+    storage.discard_abci_responses = false;
+    tx_index.indexer = cfg.txIndex.indexer;
+    tx_index.psql_conn = cfg.txIndex.psqlConn;
+    statesync.enable = false;
+  };
+
+  # Set up initial configuration directory structure, to be copied when service is run
+  configInDir = pkgs.writeTextDir "/config/config.toml" (readFile configToml);
+  genesisInDir = pkgs.writeTextDir "/config/genesis.json" (readFile cfg.genesisFile);
+  initialHomeDir = pkgs.symlinkJoin {
+    name = "penumbra-cometbft-home";
+    paths = [ configInDir genesisInDir ];
+  };
 in {
   options.penumbra.services.cometbft = {
     enable = mkEnableOption "Enables just CometBFT without automatically starting the Penumbra daemon";
@@ -188,127 +293,32 @@ in {
       default = 26660;
       description = "The port Prometheus will scrape for metrics";
     };
+
+    serviceName = mkOption {
+      type = types.str;
+      default = "cometbft";
+      description = "The name of the systemd service for CometBFT";
+    };
   };
 
   config = mkIf cfg.enable {
     # Add the cometbft executable to the environment
-    environment.systemPackages = [ self.packages.${pkgs.system}.cometbft ];
+    environment.systemPackages = [ cometbft ];
 
-    systemd.services."penumbra.cometbft" = {
+    systemd.services.${cfg.serviceName} = {
       wantedBy = ["multi-user.target"];
-      serviceConfig = with strings; let
-        # Shorthand for the package, used below
-        cometbft = self.packages.${pkgs.system}.cometbft;
-
-        # Write a config.toml file for CometBFT given the configuration options
-        configToml = (pkgs.formats.toml {}).generate "penumbra-cometbft-config" {
-          version = cometbft.version;
-          proxy_app = "tcp://${cfg.proxyApp.ip}:${toString cfg.proxyApp.port}";
-          moniker = cfg.moniker;
-          db_backend = "goleveldb";
-          db_dir = cfg.dataDir;
-          log_level = "info";
-          log_format = "plain";
-          priv_validator_laddr =
-            if cfg.privValidator.laddr.enable
-            then "tcp://${cfg.privValidator.laddr.ip}:${toString cfg.privValidator.laddr.port}"
-            else "";
-          abci = "socket";
-          filter_peers = false;
-          rpc = mkIf cfg.rpc.enable {
-            laddr = "tcp://${cfg.rpc.ip}:${toString cfg.rpc.port}";
-            cors_allowed_origins = [];
-            cors_allowed_methods = ["HEAD" "GET" "POST"];
-            cors_allowed_headers = ["Origin" "Accept" "Content-Type" "X-Requested-With" "X-Server-Time"];
-            grpc_laddr = "";
-            grpc_max_open_connections = 900;
-            unsafe = false;
-            max_open_connections = 900;
-            max_subscription_clients = 100;
-            max_subscriptions_per_client = 5;
-            timeout_broadcast_tx_commit = "10000ms";
-            max_body_bytes = 1000000;
-            max_header_bytes = 1048576;
-            tls_cert_file = "";
-            tls_key_file = "";
-            pprof_laddr = "";
-          };
-          p2p = {
-            laddr = "tcp://${cfg.p2p.ip}:${toString cfg.p2p.port}";
-            external_address = cfg.p2p.externalAddress;
-            seeds = concatStringsSep "," cfg.p2p.seeds;
-            persistent_peers = concatStringsSep "," cfg.p2p.persistentPeers;
-            addr_book_file = cfg.p2p.addrBook;
-            addr_book_strict = cfg.p2p.addrBookStrict;
-            max_num_inbound_peers = cfg.p2p.maxPeers.inbound;
-            max_num_outbound_peers = cfg.p2p.maxPeers.outbound;
-            unconditional_peer_ids = concatStringsSep "," cfg.p2p.unconditionalPeerIds;
-            persistent_peers_max_dial_period = "0s";
-            flush_throttle_timeout = "100ms";
-            max_packet_msg_payload_size = 1024;
-            send_rate = 5120000;
-            recv_rate = 5120000;
-            pex = cfg.p2p.pex;
-            seed_mode = cfg.p2p.seedMode;
-            private_peer_ids = concatStringsSep "," cfg.p2p.privatePeerIds;
-            allow_duplicate_ip = cfg.p2p.allowDuplicateIp;
-            handshake_timeout = cfg.p2p.handshakeTimeout;
-            dial_timeout = cfg.p2p.dialTimeout;
-          };
-          mempool = {
-            recheck = true;
-            broadcast = true;
-            wal_dir = "";
-            size = 5000;
-            max_txs_bytes = 1073741824;
-            cache_size = 10000;
-            keep-invalid-txs-in-cache = false;
-            max_tx_bytes = 1048576;
-            max_batch_bytes = 0;
-          };
-          consensus = {
-            wal_file = "${cfg.dataDir}/cs.wal/wal";
-            timeout_propose = "3000ms";
-            timeout_propose_delta = "500ms";
-            timeout_prevote = "1000ms";
-            timeout_prevote_delta = "500ms";
-            timeout_precommit = "1000ms";
-            timeout_precommit_delta = "500ms";
-            timeout_commit = "5000ms";
-            double_sign_check_height = cfg.consensus.doubleSignCheckHeight;
-            skip_timeout_commit = false;
-            create_empty_blocks = true;
-            create_empty_blocks_interval = "0ms";
-            peer_gossip_sleep_duration = "100ms";
-            peer_query_maj23_sleep_duration = "2000ms";
-          };
-          instrumentation = {
-            prometheus = toString cfg.prometheus.listenPort ? false;
-            prometheus_listen_addr = ":${toString cfg.prometheus.listenPort}";
-            max_open_connections = 3;
-            namespace = "cometbft";
-          };
-          storage.discard_abci_responses = false;
-          tx_index.indexer = cfg.txIndex.indexer;
-          tx_index.psql_conn = cfg.txIndex.psqlConn;
-          statesync.enable = false;
-        };
-        configInDir = pkgs.writeTextDir "/config/config.toml" (readFile configToml);
-        genesisInDir = pkgs.writeTextDir "/config/genesis.json" (readFile cfg.genesisFile);
-        initialHomeDir = pkgs.symlinkJoin {
-          name = "penumbra-cometbft-home";
-          paths = [ configInDir genesisInDir ];
-        };
-      in {
+      serviceConfig =  {
         # Restart = "on-failure";
         Restart = "no";
         # This creates a directory at `/var/lib/penumbra/cometbft` unconditionally, though it may
         # not actually be used if the home directory and/or data directory are both overridden:
         StateDirectory = "penumbra/cometbft";
+        StateDirectoryMode = "0600";
         ExecStart = ''
           ${pkgs.bash}/bin/bash -c \
             "${pkgs.coreutils}/bin/mkdir -p ${cfg.homeDir} && \
              ${pkgs.coreutils}/bin/mkdir -p ${cfg.dataDir} && \
+             ${}
              ${pkgs.coreutils}/bin/cp -rf ${initialHomeDir}/config ${cfg.homeDir} && \
              ${cometbft}/bin/cometbft init --home ${cfg.homeDir} && \
              ${cometbft}/bin/cometbft start --home ${cfg.homeDir}"
@@ -316,6 +326,7 @@ in {
         # Prevent privilege escalation
         NoNewPrivileges = "yes";
         RestrictSUIDSGID = "yes";
+        PrivateUsers = "yes";
         # Don't allow access to any user home directories
         ProtectHome = "yes";
         # Protect parts of the system from access and modification
@@ -324,6 +335,14 @@ in {
         ProtectKernelModules = "yes";
         ProtectKernelTunables = "yes";
         ProtectControlGroups = "yes";
+        ProtectClock = "yes";
+        ProtectHostname = "yes";
+        PrivateTmp = "yes";
+        # Restrict things that the service doesn't need to do
+        RestrictRealtime = "yes";
+        # Prevent dynamic code execution (and because everything is mounted noexec except the Nix
+        # store, files written to disk by the service cannot be executed to get around this)
+        MemoryDenyWriteExecute = "yes";
         # We don't use UNIX sockets, so we can disable them
         RestrictAddressFamilies = [ "AF_INET" "AF_INET6" ];
         # Only permit writes to `/run` (needed for mount points) and the data and home directories
